7804762ca4bbc7c7a593efb8504c385a6dad97d6
Paul Kilgo <paulkilgo@gmail.com>
2011-07-29 13:36:33 -0500

    Fetching of now playing info moved to separate thread

--- src/cc/rainwave/android/NowPlayingActivity.java
+++ src/cc/rainwave/android/NowPlayingActivity.java
@@ -6,11 +6,13 @@ import cc.rainwave.android.api.types.Song;
 
 import android.app.Activity;
 import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 
 public class NowPlayingActivity extends Activity {
 	private static final String TAG = "NowPlaying";
@@ -19,30 +21,52 @@ public class NowPlayingActivity extends Activity {
 	
 	private Session mSession;
 	
+	private FetchInfo mFetchInfo;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_nowplaying);
-        initialize();
     }
     
-    private void initialize() {
-    	try {
-			mSession = Session.makeSession();
-			
-			// TODO: Make this happen somewhere other than the UI thread.
-			mOrganizer = mSession.asyncGet();
-			
-			updateSchedule();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+    
+    @Override
+    public void onResume() {
+        super.onResume();
+        if(mSession == null) {
+            initializeSession();
+        }
+        
+        fetchSchedules();
+    }
+    
+    private void initializeSession() {
+        try {
+            mSession = Session.makeSession();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    private void fetchSchedules() {
+        if(mSession == null) {
+            // TODO: Some error here.
+            return;
+        }
+        
+        if(mFetchInfo == null) {
+            mFetchInfo = new FetchInfo();
+            mFetchInfo.execute(mSession);
+        }
     }
     
     private void updateSchedule() {
     	if(mOrganizer == null) return;
-    	
     	
     	Song current = mOrganizer.getCurrentSong();
     	((TextView) findViewById(R.id.np_songTitle)).setText(current.song_title);
@@ -57,5 +81,31 @@ public class NowPlayingActivity extends Activity {
     	catch(IOException e) {
     	    e.printStackTrace();
     	}
+    }
+    
+    /**
+     * Fetches the now playing info.
+     * @author pkilgo
+     *
+     */
+    protected class FetchInfo extends AsyncTask<Session, Integer, ScheduleOrganizer> {
+
+        @Override
+        protected ScheduleOrganizer doInBackground(Session... s) {
+            try {
+                return s[0].asyncGet();
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+        
+        protected void onPostExecute(ScheduleOrganizer result) {
+            super.onPostExecute(result);
+            mOrganizer = result;
+            updateSchedule();
+            mFetchInfo = null;
+        }
+        
     }
 }
