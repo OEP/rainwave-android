c986f3d696d9db07661b97f7db784c35d0142c4a
Paul Kilgo <paulkilgo@gmail.com>
2011-08-03 16:54:40 -0500

    Some commenting done

--- src/cc/rainwave/android/NowPlayingActivity.java
+++ src/cc/rainwave/android/NowPlayingActivity.java
@@ -30,15 +30,26 @@ import android.widget.TextView;
 import java.io.IOException;
 import java.net.MalformedURLException;
 
+/**
+ * This is the primary activity for this application. It announces
+ * which song is playing, handles ratings, and also elections.
+ * @author pkilgo
+ *
+ */
 public class NowPlayingActivity extends Activity {
+    /** Debug tag */
 	private static final String TAG = "NowPlaying";
 	
+	/** This is the last response from the last schedule sync */
 	private RainwaveResponse mOrganizer;
 	
+	/** This manages our connection with the Rainwave server */
 	private Session mSession;
 	
+	/** AsyncTask for schedule syncs */
 	private FetchInfo mFetchInfo;
 	
+	/** AsyncTask for song ratings */
 	private RateTask mRateTask;
 	
     /** Called when the activity is first created. */
@@ -54,13 +65,22 @@ public class NowPlayingActivity extends Activity {
         super.onStart();
     }
     
+    /**
+     * Our strategy here is to attempt to re-initialize the
+     * app as much as possible. This helps us to catch preference
+     * changes, and to not have lingering song data lying around.
+     */
     @Override
     public void onResume() {
         super.onResume();
         initializeSession();
-        fetchSchedules();
+        initSchedules();
     }
 
+    /**
+     * We also want to stop our threads as much as possible, as they
+     * should solely run in the foreground.
+     */
     public void onPause() {
     	super.onPause();
     	stopTasks();
@@ -73,11 +93,16 @@ public class NowPlayingActivity extends Activity {
     public void onDestroy() {
     	super.onDestroy();
     }
-    
+
+    /**
+     * Dialog manufacturer.
+     */
     public Dialog onCreateDialog(int id) {
     	AlertDialog.Builder builder = new AlertDialog.Builder(this);
     	
     	switch(id) {
+    	    
+    	// The 'rate song' dialog.
     	case DIALOG_RATE:
     		final RatingBar rating = new RatingBar(this);
     		rating.setStepSize(0.5f);
@@ -97,11 +122,16 @@ public class NowPlayingActivity extends Activity {
     			.create();
     		
     	default:
+    	    // Programmer specified invalid dialog ID.
     		return builder.setMessage("Sorry! Your princess is in another castle!").create();
     	}
     }
     
+    /**
+     * Sets up listeners for this activity.
+     */
     private void setListeners() {
+        // Pops up the rating dialog if we are authenticated.
     	OnTouchListener tmp = new OnTouchListener() {
 			@Override
 			public boolean onTouch(View v, MotionEvent e) {
@@ -116,10 +146,15 @@ public class NowPlayingActivity extends Activity {
 			}
     	};
     	
+    	// The rating dialog should show up if the Song/Album rating TextView is clicked.
     	((TextView) findViewById(R.id.np_songRating)).setOnTouchListener(tmp);
     	((TextView) findViewById(R.id.np_albumRating)).setOnTouchListener(tmp);
     }
     
+    /**
+     * Stops ALL AsyncTasks and removes
+     * all references to them.
+     */
     private void stopTasks() {
     	if(mFetchInfo != null) {
     		mFetchInfo.cancel(true);
@@ -132,15 +167,30 @@ public class NowPlayingActivity extends Activity {
     	}
     }
     
-    private void fetchSchedules() {
+    /**
+     * Performs an initial (e.g., non-longpoll) fetch
+     * of our song info.
+     */
+    private void initSchedules() {
         fetchSchedules(true);
     }
     
+    /**
+     * Performs a long-polling synchronous update
+     * of our song info.
+     */
     private void syncSchedules() {
         fetchSchedules(false);
     }
     
+    /**
+     * Performs an update of song info.
+     * @param init flag to indicate this
+     *   is an initial (non-long-poll) fetch.
+     */
     private void fetchSchedules(boolean init) {
+        // Some really bad thing happened and we don't
+        // have a connection at all.
         if(mSession == null) {
             // TODO: Some error here.
             return;
@@ -152,6 +202,7 @@ public class NowPlayingActivity extends Activity {
         }
     }
     
+    /** Shows the menu */
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		MenuInflater inflater = getMenuInflater();
@@ -159,16 +210,26 @@ public class NowPlayingActivity extends Activity {
 		return true;
 	}
 
+	/** Responds to menu selection */
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch(item.getItemId()) {
+		    
+		// Start RainwavePreferenceActivity.
 		case R.id.menu_preferences:
 			Intent i = new Intent(this, RainwavePreferenceActivity.class);
 			startActivity(i);
 			break;
 		}
+		
 		return false;
 	}
     
+	/**
+	 * Destroys any existing Session and creates
+	 * a new Session object for us to use, pulling
+	 * the user_id and key attributes from the default
+	 * Preference store.
+	 */
     private void initializeSession() {
         try {
             mSession = Session.makeSession(this);
@@ -177,22 +238,38 @@ public class NowPlayingActivity extends Activity {
         }
     }
     
+    /**
+     * Executes when a schedule sync finished.
+     * @param response the response the server issued
+     */
     private void onScheduleSync(RainwaveResponse response) {
     	if(response == null) {
     	    // TODO: Some error here.
     	    return;
     	}
     	
+    	// Updates title, album, and artists.
     	updateSongInfo(response.getCurrentSong());
+    	
+    	// Updates song, album ratings.
     	setRatings(response.getCurrentSong());
     }
     
+    /**
+     * Updates the song title, album title, and
+     * artists in the user interface.
+     * @param current the current song that's playing.
+     */
     private void updateSongInfo(Song current) {
     	((TextView) findViewById(R.id.np_songTitle)).setText(current.song_title);
     	((TextView) findViewById(R.id.np_albumTitle)).setText(current.album_name);
     	((TextView) findViewById(R.id.np_artist)).setText(current.collapseArtists());
     }
     
+    /**
+     * Updates the song and album ratings.
+     * @param current the current song playing
+     */
     private void setRatings(Song current) {
     	((TextView) findViewById(R.id.np_songRating))
     	   .setText(getRatingString(current.song_rating_user, current.song_rating_avg));
@@ -201,19 +278,37 @@ public class NowPlayingActivity extends Activity {
  	       .setText(getRatingString(current.album_rating_user, current.album_rating_avg));
     }
     
+    /**
+     * Executes when a "rate song" request has finished.
+     * @param result the result the server issued
+     */
     private void onRateSong(RatingResult result) {
         mOrganizer.updateSongRatings(result);
         setRatings(mOrganizer.getCurrentSong());
     }
     
+    /**
+     * Builds a rating String string from the user rating
+     * and average rating of the format: UU/AA, where UU
+     * is the user rating or "--" if nonexistant and AA
+     * is the average rating or "--" if nonexistant.
+     * @param user the user's rating
+     * @param avg the average rating
+     * @return the rating string
+     */
     private String getRatingString(float user, float avg) {
     	StringBuilder sb = new StringBuilder();
     	sb.append((user >= 1.0f) ? String.format("%1.1f", user) : "--");
     	sb.append("/");
-    	sb.append((user >= 1.0f) ? String.format("%1.1f", avg) : "--");
+    	sb.append((avg >= 1.0f) ? String.format("%1.1f", avg) : "--");
     	return sb.toString();
     }
     
+    /**
+     * Sets the album art to the provided Bitmap, or
+     * a default image if art is null.
+     * @param art desired album art
+     */
     private void updateAlbumArt(Bitmap art) {
         if(art == null) {
             // TODO: Some error here.
@@ -223,6 +318,13 @@ public class NowPlayingActivity extends Activity {
         ((ImageView) findViewById(R.id.np_albumArt)).setImageBitmap(art);
     }
     
+    /**
+     * AsyncTask for submitting a rating for a song.
+     * Expects two arguments to <code>execute(Object...params)</code>,
+     * which are song_id (int), and rating (float).
+     * @author pkilgo
+     *
+     */
     protected class RateTask extends AsyncTask<Object, Integer, RatingResult> {
 		@Override
 		protected RatingResult doInBackground(Object ... params) {
@@ -251,6 +353,9 @@ public class NowPlayingActivity extends Activity {
     
     /**
      * Fetches the now playing info.
+     * Expects one argument to <code>execute(Object...params)</code> which
+     * is the flag to indicate if this is an initializing (e.g., non-longpoll)
+     * fetch of the schedule data.
      * @author pkilgo
      *
      */
@@ -304,6 +409,7 @@ public class NowPlayingActivity extends Activity {
             
             mOrganizer = result.getParcelable(SCHEDULE);
             
+            // Callback for schedule sync.
             onScheduleSync(mOrganizer);
             updateAlbumArt( (Bitmap) result.getParcelable(ART) );
             
