0231afce55c0bd127c3c8d0da142c65eddb0172b
Paul Kilgo <paulkilgo@gmail.com>
2011-08-09 01:09:33 -0500

    Fixed where API keys couldnt be entered properly

--- src/cc/rainwave/android/RainwavePreferenceActivity.java
+++ src/cc/rainwave/android/RainwavePreferenceActivity.java
@@ -1,13 +1,22 @@
 package cc.rainwave.android;
 
 import android.os.Bundle;
+import android.preference.EditTextPreference;
 import android.preference.PreferenceActivity;
+import android.widget.EditText;
 
 public class RainwavePreferenceActivity extends PreferenceActivity {
 
 		public void onCreate(Bundle icicle) {
 			super.onCreate(icicle);
 			addPreferencesFromResource(R.xml.preferences);
+			setupUI();
+		}
+		
+		private void setupUI() {
+		    EditTextPreference key = (EditTextPreference) findPreference(Rainwave.PREFS_KEY);
+		    EditText field = key.getEditText();
+		    field.setKeyListener(new HexadecimalKeyListener());
 		}
 	
 }
