5a596fd7501fb90559692a53792bcc67a1152994
Paul Kilgo <paulkilgo@gmail.com>
2011-11-14 22:42:55 -0600

    Layout started for Playlist Manager

---
+++ src/cc/rainwave/android/PlaylistActivity.java
@@ -0,0 +1,12 @@
+package cc.rainwave.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PlaylistActivity extends Activity {
+	
+	public void onCreate(Bundle icicle) {
+		super.onCreate(icicle);
+	}
+	
+}
